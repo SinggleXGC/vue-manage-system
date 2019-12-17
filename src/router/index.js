9e0151333e2dcf09bce58c62efb375378d8039eb
SinggleXGC <1307708161@qq.com>
2019-12-17 22:22:51 +0800

    vue开发管理后台--实验2 创建组件配置路由 -- 配置简化

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import vHome from '../components/common/Home.vue'
 import vLogin from '../components/page/Login.vue'
-import vChild from '../components/page/Children.vue'
 
 Vue.use(VueRouter);
 
@@ -13,13 +11,24 @@ const routes = [
         //设置URL
         path: '/home',
         //设置对应组件
-        component: vHome,
+        component: () => import('../components/common/Home.vue'),
         children: [
             {
                 //要注意，以 / 开头的嵌套路径会被当作根路径。
                 // 使用嵌套组件无须设置嵌套的路径。
                 path: 'child',
-                component: vChild
+                component: () => import('../components/page/Children.vue'),
+            },
+            {
+                path: 'child1',
+                component: () => import('../components/page/Children1.vue'),
+            },
+            {
+                path: 'child2',
+                component: () => import('../components/page/Children2.vue'),
+            },{
+                path: 'child3',
+                component: () => import('../components/page/Children3.vue'),
             }
         ],
         //设置相应元信息
